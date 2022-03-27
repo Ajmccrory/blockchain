52736f47ad8a7425c10c37728abea30e83cc1cfe
aj <93270610+Ajmccrory@users.noreply.github.com>
2022-03-27 00:17:10 -0400

    Update util.go

--- blockchain/wallet/util.go
+++ blockchain/wallet/util.go
@@ -1,75 +1,21 @@
 package wallet
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/sha256"
 	"log"
 
-	"golang.org/x/crypto/ripemd160"
+	"github.com/mr-tron/base58"
 )
 
-const (
-	checksumLength = 4
-	//hexadecimal representation of 0
-	version = byte(0x00)
-)
+func base58Encode(input []byte) []byte {
+	encode := base58.Encode(input)
 
-type Wallet struct {
-	//ecdsa = eliptical curve digital signiture algorithm
-	PrivateKey ecdsa.PrivateKey
-	PublicKey  []byte
+	return []byte(encode)
 }
 
-func NewKeyPair() (ecdsa.PrivateKey, []byte) {
-	curve := elliptic.P256()
-
-	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+func base58Decode(input []byte) []byte {
+	decode, err := base58.Decode(string(input[:]))
 	if err != nil {
 		log.Panic(err)
 	}
-
-	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
-
-	return *private, pub
-}
-
-func MakeWallet() *Wallet {
-	privateKey, publicKey := NewKeyPair()
-	wallet := Wallet{privateKey, publicKey}
-	return &wallet
-}
-
-func PublicKeyHash(publicKey []byte) []byte {
-	hashedPublicKey := sha256.Sum256(publicKey)
-
-	hasher := ripemd160.New()
-	_, err := hasher.Write(hashedPublicKey[:])
-	if err != nil {
-		log.Panic(err)
-	}
-	publicRipeMd := hasher.Sum(nil)
-
-	return publicRipeMd
-}
-
-func Checksum(ripeMdHash []byte) []byte {
-	firstHash := sha256.Sum256(ripeMdHash)
-	secondHash := sha256.Sum256(firstHash[:])
-
-	return secondHash[:checksumLength]
-}
-
-func (w *Wallet) Address() []byte {
-	pubHash := PublicKeyHash(w.PublicKey)
-
-	versionedHash := append([]byte{version}, pubHash...)
-
-	checksum := Checksum(versionedHash)
-
-	finalHash := append(versionedHash, checksum...)
-	address := base58Encode(finalHash)
-
-	return address
+	return decode
 }
